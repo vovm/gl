e3092396612e4eb0bee22a250a08f95c05b7df42
vova <vova@vova.ua>
2017-12-27 01:19:39 +0200

    6th task

--- 06_linux.sh
+++ 06_linux.sh
@@ -3,3 +3,5 @@
 # create a minimal bash/sh script returned a names of local files
 # what were changed yourself into a current folder for last week.
 # A result has to be sorted by changed date.
+
+find . -type f -mtime -7 -printf '%T+ %u %g %TR %TD %p\n' | sort -r | grep $(whoami)
